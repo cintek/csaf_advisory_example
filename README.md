697b39f40cc3e075dd9bb5851b7ea4517fc3c8e3
Christoph Klassen <christoph.klassen@intevation.de>
2023-09-13 10:34:40 +0200

    changed the example to search for PURLs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # csaf_advisory_example
 
-This is an example for how to use the [advisory model](https://github.com/cintek/csaf_distribution/blob/main/csaf/advisory.go) to change an advisory of the CSAF 2.0 standard. To be specific it changes the category of every branch to "legacy".
+This is an example for how to use the [advisory model](https://github.com/cintek/csaf_distribution/blob/main/csaf/advisory.go) to search for PURLs belonging to a product ID in an advisory of the CSAF 2.0 standard.
 
 ## How to use
 
@@ -8,4 +8,7 @@ Build the binary:
 `go build`
 
 Run the binary with one or more files as parameters:
-`csaf_advisory_example [files]`
+`csaf_advisory_example [OPTIONS] [files]`
+
+Find out how to use the options by running:
+`csaf_advisory_example --help`
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
 
 	"github.com/csaf-poc/csaf_distribution/v2/csaf"
@@ -15,9 +16,10 @@ import (
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(),
-			"Usage:\n  %s files...\n", os.Args[0])
+			"Usage:\n  %s [OPTIONS] files...\n\nOptions:\n", os.Args[0])
 		flag.PrintDefaults()
 	}
+	idsString := flag.String("p", "", "ID1,ID2,...")
 	flag.Parse()
 
 	files := flag.Args()
@@ -25,40 +27,103 @@ func main() {
 		log.Println("No files given.")
 		return
 	}
-	if err := run(files); err != nil {
+	if err := run(files, *idsString); err != nil {
 		log.Fatalf("error: %v\n", err)
 	}
 }
 
-// run opens files, calls function to edit them and saves them as different files.
-func run(files []string) error {
+// idPurlDict stores a product id and the PURLs belonging to the product.
+type idPurlDict struct {
+	ID    string
+	PURLs []string
+}
 
+// idPurlDicts is a list of idPurlDict elements.
+type idPurlDicts []idPurlDict
+
+// Add adds the given PURLs to a idPurlDict which has the given Product ID.
+func (dicts idPurlDicts) Add(id string, purls ...string) idPurlDicts {
+	for i, d := range dicts {
+		if d.ID == id {
+			dicts[i].PURLs = append(d.PURLs, purls...)
+			return dicts
+		}
+	}
+	newDict := idPurlDict{
+		ID:    id,
+		PURLs: purls,
+	}
+
+	return append(dicts, newDict)
+}
+
+// run prints PURLs belonging to the given Product IDs.
+func run(files []string, idsString string) error {
 	for _, file := range files {
 		adv, err := csaf.LoadAdvisory(file)
 		if err != nil {
 			return fmt.Errorf("loading %q failed: %w", file, err)
 		}
 
-		if adv.ProductTree != nil {
-			changeBranchCategoryToLegacy(adv.ProductTree.Branches)
-		}
-
-		file = strings.TrimSuffix(file, ".json") + "_new.json"
-
-		if err := csaf.SaveAdvisory(adv, file); err != nil {
-			return fmt.Errorf("saving %q failed: %w", file, err)
+		if idsString != "" {
+			ids := strings.Split(idsString, ",")
+			dict := findProductPackageUrls(adv, ids)
+			fmt.Println("Found the following PURLs")
+			for _, d := range dict {
+				fmt.Printf("Product ID %s:\n", d.ID)
+				for i, p := range d.PURLs {
+					fmt.Printf("%d. %s\n", i+1, p)
+				}
+				fmt.Println()
+			}
 		}
 	}
 
 	return nil
 }
 
-// Change the category of every branch in the product tree to "legacy"
-func changeBranchCategoryToLegacy(branches []*csaf.Branch) {
-	for _, branch := range branches {
-		branch.Category = csaf.CSAFBranchCategoryLegacy
-		if branch.Branches != nil {
-			changeBranchCategoryToLegacy(branch.Branches)
+// findProductPackageUrls uses the given (product) ids to find the appropriate
+// PURLs in the product tree of the given CSAF advisory.
+func findProductPackageUrls(adv *csaf.Advisory, ids []string) []idPurlDict {
+	var dict idPurlDicts
+	if tree := adv.ProductTree; tree != nil {
+		if names := tree.FullProductNames; names != nil {
+			for _, name := range *names {
+				if slices.Contains(ids, string(*name.ProductID)) {
+					if helper := name.ProductIdentificationHelper; helper == nil {
+						if helper.PURL != nil {
+							dict = dict.Add(string(*name.ProductID), string(*helper.PURL))
+						}
+					}
+				}
+			}
+		}
+		if branches := tree.Branches; branches != nil {
+			newURLs := findPURLsInBranches(branches, ids)
+			dict = append(dict, newURLs...)
 		}
 	}
+	return dict
+}
+
+// findPURLsInBranches uses the given (product) ids to find the appropriate
+// PURLs in a list of branches of a CSAF advisory.
+func findPURLsInBranches(branches csaf.Branches, ids []string) []idPurlDict {
+	var dict idPurlDicts
+	for _, branch := range branches {
+		if name := branch.Product; name != nil {
+			if slices.Contains(ids, string(*name.ProductID)) {
+				if helper := name.ProductIdentificationHelper; helper != nil {
+					if helper.PURL != nil {
+						dict = dict.Add(string(*name.ProductID), string(*helper.PURL))
+					}
+				}
+			}
+		}
+		if branch.Branches != nil {
+			newURLs := findPURLsInBranches(branch.Branches, ids)
+			dict = append(dict, newURLs...)
+		}
+	}
+	return dict
 }
